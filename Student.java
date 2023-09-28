57024e6c42033b649389cfb14663deccb266b524
law <lawrenze3206@gmail.com>
2023-09-28 20:26:43 +0800

    commit

---
+++ Student.java
@@ -0,0 +1,26 @@
+package Student;
+
+public class Student {
+	    private String name;
+	    private String section;
+	    private String motto;
+
+	    public Student(String name, String section, String motto) {
+	        this.name = "nalan";
+	        this.section = section;
+	        this.motto = motto;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public String getSection() {
+	        return section;
+	    }
+
+	    public String getMotto() {
+	        return motto;
+	    }
+	}
+
---
+++ main.java
@@ -0,0 +1,13 @@
+package Student;
+
+public class main {
+	
+	    public static void main(String[] args) {
+	 
+	      Student student1 = new Student("Shaina Jean M. Heras","C","Whatever will be, will be :)");
+	        
+	        System.out.println("Name: " + student1.getName());
+	        System.out.println("Section: " + student1.getSection());
+	        System.out.println("Motto: " + student1.getMotto());
+	    }
+}
